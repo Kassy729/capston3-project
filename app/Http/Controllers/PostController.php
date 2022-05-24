1ce2e424f6c593623f7a76b145096530db0cb36e
Kassy729 <hamtol98@gmail.com>
2022-05-24 13:22:15 +0900

    test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -248,6 +248,7 @@ class PostController extends Controller
         //팔로잉한 아이디의 포스트만 시간별로 출력
         $post = Post::with(['user', 'likes', 'image'])->whereIn('user_id', $array)->where('range', 'public')->orderby('updated_at', 'desc')->paginate(10);
 
+
         $opponent_post = array();
         $opponent_user = array();
         $array = array();
@@ -275,7 +276,6 @@ class PostController extends Controller
                 array_push($array2, []);
             }
             $post[$i]['likeCheck'] = in_array($id, $array2[$i]);
-            return $post;
 
             // 댓글 개수 체크
             $comments = Comment::where('post_id', '=', $post[$i]->id)->get();
