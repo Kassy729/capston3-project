4fce634328603c998f973e90d85f2aa3bfa364f5
Kassy729 <hamtol98@gmail.com>
2022-05-24 13:09:44 +0900

    test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -257,16 +257,14 @@ class PostController extends Controller
 
         for ($i = 0; $i < count($post); $i++) {
             if ($post[$i]->opponent_id) {
-                return 1;
                 $op_post = Post::where('id', '=', $post[$i]->opponent_id)->first();
                 $op_user = User::where('id', '=', $op_post->user_id)->first();
                 array_push($opponent_post, $op_post);
                 array_push($opponent_user, $op_user);
                 $post[$i]['opponent_post'] = $opponent_post[$i];
                 $post[$i]['opponent_post']['user'] = $opponent_user[$i];
-            } else {
-                return 2;
             }
+            return 1;
 
             //좋아요 체크
             if (count($post[$i]->likes) !== 0) {
