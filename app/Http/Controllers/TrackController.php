371f62abe883908b5260d8e37e2b6f2742ef39f0
Kassy729 <hamtol98@gmail.com>
2022-05-26 21:29:17 +0900

    track-profile

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -52,7 +52,7 @@ class TrackController extends Controller
         $track = json_decode($response, true);
 
         $profile = User::where('id', '=', $track['user']['userId'])->first('profile');
-        $track['user'] = $profile;
+        $track['user']['profile'] = $profile->profile;
 
         if ($track) {
             return response($track, 200);
